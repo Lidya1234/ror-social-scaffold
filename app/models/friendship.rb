93b5dd0c4128d0935e2bbe855f1981b6ba044977
Lidya1234 <lidya94gher@gmail.com>
2020-12-16 13:48:15 +0300

    Remove make duplicate from frienship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -10,7 +10,7 @@ class Friendship < ApplicationRecord
         ->(user, friend) { where("( user_id = #{user.id} AND friend_id = #{friend.id}) OR ( user_id = #{friend.id} AND friend_id = #{user.id})") }
 
   before_create :check_friendship
-  # after_create :make_duplicate
+ 
 
   def check_friendship
     if Friendship.friendship_exists(User.find(user_id), User.find(friend_id)).to_a.any?
@@ -19,8 +19,6 @@ class Friendship < ApplicationRecord
     true
   end
 
-  def make_duplicate
-    Friendship.create(user_id: friend_id, friend_id: user_id, requester_id: user_id)
-  end
+
 end
 # rubocop: enable Layout/LineLength
